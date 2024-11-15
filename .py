231ce28fc7a673ba14521098bec5fc955cb1e329
AQEELJOHNSAHOTRA <aqeeljohnsahotra@gmail.com>
2024-11-15 15:36:34 +0500

    Simple Calculator .py
    AQEEL JOHN SAHOTRA:
    Simple Calculator for Addition, Subtraction, Multiplication and Division.

---
+++ .py
@@ -0,0 +1,12 @@
+operator= input("Enter operator(+,-,*,/): ")
+n1= float(input("Enter n1: "))
+n2= float(input("Enter n2: "))
+
+if operator=="+":
+    print(f" Sum of {n1} and {n2} is {n1+n2}")
+elif operator=="-":
+     print(f" subtraction of {n1} and {n2} is {n1-n2}")
+elif operator=="*":
+     print(f" Multiplication of {n1} and {n2} is {n1*n2}")
+elif operator=="/":
+     print(f" Division of {n1} and {n2} is {n1/n2}")
